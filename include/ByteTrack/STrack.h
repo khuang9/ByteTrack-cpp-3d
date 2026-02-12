c66b33e625c852719e53bf5d8d575a872439af1b
khuang9 <khuangfirst@gmail.com>
2026-02-12 02:41:19 -0500

    Set default true for use_maj_cls

--- include/ByteTrack/STrack.h
+++ include/ByteTrack/STrack.h
@@ -17,7 +17,7 @@ enum class STrackState {
 class STrack
 {
 public:
-    STrack(const Rect<float>& rect, const float& score, const int& class_id, const bool& use_maj_cls);
+    STrack(const Rect<float>& rect, const float& score, const int& class_id, const bool& use_maj_cls = true);
     ~STrack();
 
     const Rect<float>& getRect() const;
